0760eda4bc3879fbb0e2d08f7c292ed2ec718e6e
arnab1896 <arnab1896@gmail.com>
2018-05-23 17:53:49 +0530

    minor changes

--- python_query_respond/py.py
+++ python_query_respond/py.py
@@ -1,5 +1,6 @@
 ##test code
 
+
 f = open('test_in_csv.csv','r')
 lines=f.readlines()
 a = ''
@@ -56,7 +57,7 @@ for x in range(0, count_comma):
 
 ##defining macros for table headers
 
-parent_cell = 1
+"""parent_cell = 1
 x_pc = 2
 y_pc = 3
 z_pc = 4
@@ -67,32 +68,4 @@ z_1 = 8
 daughter_2 = 9
 x_2 = 10
 y_2 = 11
-z_2 = 12
-
-
-
-#print ('\x1b[5;30;43m' + "Parent Cell and it's parameters:" + '\x1b[0m')
-#print ("Name = " + lines[int(line_no)][parent_cell] + lines[int(line_no)][parent_cell+1])
-
-#print ('\x1b[5;30;43m' + "Daughter Cell 1 and it's parameters:" + '\x1b[0m')
-
-
-#print ('\x1b[5;30;43m' + "Daughter Cell 2 and it's parameters:" + '\x1b[0m')
-
-
-
-
-
-
-
-
-
-
-
-
-#print the root element Cell
-#print ('\x1b[5;30;43m' + "<Cell>" + '\x1b[0m')
-#close the root element /Cell
-#print ('\x1b[5;30;43m' + "</Cell>" + '\x1b[0m')
-
-#print ('\x1b[6;30;41m' + "<name>" + '\x1b[0m' + lines[1][1] + '\x1b[6;30;41m' + "</name>" + '\x1b[0m')
+z_2 = 12"""
