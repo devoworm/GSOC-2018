64335b97ce0143a134434445c9d66984f05e3dda
arnab1896 <arnab1896@gmail.com>
2018-06-27 16:56:43 +0530

    Changes made to spliting of lineage into nodes

--- python_graph_generator/nodify_cells_based_on_groups/split_lineage_to_nodes.py
+++ python_graph_generator/nodify_cells_based_on_groups/split_lineage_to_nodes.py
@@ -1,30 +1,36 @@
 lf = open('lineage_name.txt','r')
 line_lu=lf.readlines()
 
-s = line_lu[0][0:int(len(line_lu[0])-1)]
-list_of_nodes = []
+print "No. of lineages present :" + str(len(line_lu)) + "\n"
 
-length = len(s)
+for var in range (len(line_lu)):
+	s = line_lu[var][0:int(len(line_lu[var])-1)]
+	list_of_nodes = []
 
-print "Length of lineage : " + str(length)
+	length = len(s)
 
-does_space_exist = 0
+	print "____________/  ----------- \____________"
+	print "Length of lineage : " + str(length)
+	print "The lineage is : " + s
 
-for i in range (1,length) :
-    if s[i] == ' ':
-        does_space_exist = i
+	does_space_exist = 0
 
-print "\nDoes space exist?"
-if does_space_exist == 0 :
-    print "No"
-else :
-    print "Yes, at index no. " + str(does_space_exist)
+	for i in range (1,length) :
+		if s[i] == ' ':
+		    does_space_exist = i
 
-if does_space_exist > 0 :
-    list_of_nodes.append(s[0:does_space_exist])
-    for i in range ( 1,length - (does_space_exist) ) :
-        list_of_nodes.append( s[0:does_space_exist] + s[does_space_exist+1:does_space_exist+1+i] )
-else :
-    print "\nError in input format"
+	print "Does space exist?"
+	if does_space_exist == 0 :
+		print "No"
+	else :
+		print "Yes, at index no. " + str(does_space_exist)
 
-print list_of_nodes
+	if does_space_exist > 0 :
+		list_of_nodes.append(s[0:does_space_exist])
+		for i in range ( 1,length - (does_space_exist) ) :
+		    list_of_nodes.append( s[0:does_space_exist] + s[does_space_exist+1:does_space_exist+1+i] )
+	else :
+		print "\nError in input format"
+
+	print list_of_nodes
+	print "\n"
